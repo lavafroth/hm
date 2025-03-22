87ee7a25cd64ca3918b2db721fd9e41a46c33f97
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-03-22 12:37:09 +0530

    deps: remove flake-parts in favor of `lib.genAttrs`

--- flake.nix
+++ flake.nix
@@ -1,33 +1,30 @@
 {
   description = "github:lavafroth/hm CLI utility to render manim animations on save";
 
-  inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
-      perSystem =
-        { pkgs, ... }:
-        {
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [ stdenv.cc.cc.lib ];
-            LD_LIBRARY_PATH = pkgs.stdenv.cc.cc.lib.LIBRARY_PATH;
-          };
-          packages.default = pkgs.pkgsStatic.rustPlatform.buildRustPackage {
-            pname = "hm";
-            version = "1.0.0";
-            src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
-          };
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
+    in
+
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ stdenv.cc.cc.lib ];
+          LD_LIBRARY_PATH = pkgs.stdenv.cc.cc.lib.LIBRARY_PATH;
         };
+      });
+      packages = forAllSystems (pkgs: {
+        default = pkgs.pkgsStatic.rustPlatform.buildRustPackage {
+          pname = "hm";
+          version = "1.0.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+      });
     };
 }
